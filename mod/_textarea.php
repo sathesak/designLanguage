a1df31787cf1d3e89de0d90e063c599abbba47c2
Satheswaran Arumaichandran <sarumaichandran@akcolours.com>
2021-12-26 11:19:53 +0000

    latest update on require js

---
+++ mod/_textarea.php
@@ -0,0 +1,13 @@
+<section>
+    <h2>
+        Text area
+    </h2>
+
+    <div>
+        <label for="txtArea">
+            This is text area <br>
+            <textarea id="txtArea" cols="30" rows="5" class="txtFld"></textarea>
+        </label>
+    </div>
+    
+</section>
